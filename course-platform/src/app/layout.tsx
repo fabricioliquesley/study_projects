8258588c88fc4c42b3b6b6d9eceb388a98c89f89
Fabricio Liquesley <fabricioliquesley15@gmail.com>
2026-01-15 21:57:08 -0300

    course-platform: add Clerk

--- course-platform/src/app/layout.tsx
+++ course-platform/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "@/styles/globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+// import { ptBR } from "@clerk/localizations";
 
 const nunito = Nunito({
   variable: "--font-nunito-sans",
@@ -18,12 +20,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={cn(nunito.variable, "font-nunito-sans dark antialiased")}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={cn(nunito.variable, "font-nunito-sans dark antialiased")}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
