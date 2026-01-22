f70f31128f94488a4b44969ba327c2af78d8fc29
Fabricio Liquesley <fabricioliquesley15@gmail.com>
2026-01-22 11:37:22 -0300

    devstore: create a skeleton component

---
+++ devstore/src/components/skeleton.tsx
@@ -0,0 +1,11 @@
+import { cn } from "@/lib/utils";
+import { ComponentProps } from "react";
+
+export function Skeleton({ className, ...props }: ComponentProps<"div">) {
+  return (
+    <div
+      className={cn("animate-pulse rounded-md bg-zinc-50/10", className)}
+      {...props}
+    />
+  );
+}
