31c36ce7f3400658eccac1703ae6d2c50ce41512
Fabricio Liquesley <fabricioliquesley15@gmail.com>
2026-01-21 22:47:03 -0300

    devstore: create the product component

---
+++ devstore/src/components/product/index.tsx
@@ -0,0 +1,34 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+
+interface ProductProps {
+  children: React.ReactNode;
+  href: string;
+  src: string;
+  alt?: string;
+  className?: string;
+}
+
+export function Product({ children, href, src, alt, className }: ProductProps) {
+  return (
+    <Link
+      href={href}
+      className={cn(
+        "group relative col-span-6 row-span-6 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900",
+        className,
+      )}
+    >
+      <Image
+        src={src}
+        className="transition-transform duration-500 group-hover:scale-105"
+        width={920}
+        height={920}
+        quality={100}
+        alt={alt || ""}
+      />
+
+      {children}
+    </Link>
+  );
+}
