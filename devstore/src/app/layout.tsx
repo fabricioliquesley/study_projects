b448826a774a924d66b746ba95b4de07c5389ab0
Fabricio Liquesley <fabricioliquesley15@gmail.com>
2026-01-21 21:35:13 -0300

    devstore: change the style of the root layout

--- devstore/src/app/layout.tsx
+++ devstore/src/app/layout.tsx
@@ -17,8 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.variable} antialiased`}>{children}</body>
+    <html lang="pt-br">
+      <body
+        className={`${inter.variable} bg-zinc-950 text-zinc-50 antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
