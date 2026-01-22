285045e2df8c845b4e17081aac26eb1dec0a1687
Fabricio Liquesley <fabricioliquesley15@gmail.com>
2026-01-22 10:57:58 -0300

    devstore: fix environment variable name ambiguity in env validator

--- devstore/src/lib/api.ts
+++ devstore/src/lib/api.ts
@@ -1,8 +1,8 @@
 import { env } from "@/env";
 
 export const api = (path: string, init?: RequestInit) => {
-  const baseURL = env.NEXT_PUBLIC_API_URL;
-  const url = new URL(path, baseURL);
+  const baseURL = env.NEXT_PUBLIC_API_BASE_URL;
+  const url = new URL("/api".concat(path), baseURL);
 
   return fetch(url, init);
 };
