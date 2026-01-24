faa92674ba91927d21276ea6f82f1001415701d3
Fabricio Liquesley <fabricioliquesley15@gmail.com>
2026-01-24 10:56:03 -0300

    devstore: makes the component product more flexible

--- devstore/src/app/(store)/(home)/page.tsx
+++ devstore/src/app/(store)/(home)/page.tsx
@@ -17,6 +17,7 @@ export default async function Home() {
         href={`/product/${highlightedProduct.slug}`}
         src={highlightedProduct.image}
         alt={highlightedProduct.title}
+        className="col-span-6 row-span-6"
       >
         <ProductBadge
           title={highlightedProduct.title}
