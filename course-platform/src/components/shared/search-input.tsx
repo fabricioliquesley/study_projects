b340a0182e6fe76daf64f80b0bc0f3f3fd126db1
Fabricio Liquesley <fabricioliquesley15@gmail.com>
2026-01-28 23:45:02 -0300

    course-platform: add icon to search input component

--- course-platform/src/components/shared/search-input.tsx
+++ course-platform/src/components/shared/search-input.tsx
@@ -1,9 +1,21 @@
+"use client";
+
+import { Search } from "lucide-react";
 import { Input } from "../ui/input";
+import { useState } from "react";
 
 export function SearchInput() {
+  const [query, setQuery] = useState("");
+
   return (
     <div className="relative max-w-[400px] flex-1">
-      <Input className="h-9" placeholder="Search for a course" />
+      <Input
+        className="peer h-9 pl-9"
+        placeholder="Search for a course"
+        value={query}
+        onChange={({ target }) => setQuery(target.value)}
+      />
+      <Search className="text-muted-foreground peer-focus:text-primary absolute top-1/2 left-3 -translate-y-1/2 transition-all" />
     </div>
   );
 }
