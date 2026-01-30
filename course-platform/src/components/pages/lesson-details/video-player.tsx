9c14c19f49fc1cb8374fcc8b99fa6c40cc2351a3
Fabricio Liquesley <fabricioliquesley15@gmail.com>
2026-01-30 14:35:46 -0300

    course-platform: create the Video Player component

---
+++ course-platform/src/components/pages/lesson-details/video-player.tsx
@@ -0,0 +1,31 @@
+// Base styles for media player and provider (~400B).
+import "@vidstack/react/player/styles/base.css";
+import "@vidstack/react/player/styles/plyr/theme.css";
+
+import { MediaPlayer, MediaProvider } from "@vidstack/react";
+import {
+  PlyrLayout,
+  plyrLayoutIcons,
+} from "@vidstack/react/player/layouts/plyr";
+
+interface VideoPlayerProps {
+  videoId: string;
+  autoplay: boolean;
+}
+
+function VideoPlayer({ videoId, autoplay }: VideoPlayerProps) {
+  const userAlreadyInteracted = navigator.userActivation.hasBeenActive;
+
+  return (
+    <MediaPlayer
+      title="class video"
+      src={`https://www.youtube.com/${videoId}`}
+      autoPlay={autoplay && userAlreadyInteracted}
+    >
+      <MediaProvider />
+      <PlyrLayout icons={plyrLayoutIcons} />
+    </MediaPlayer>
+  );
+}
+
+export default VideoPlayer;
