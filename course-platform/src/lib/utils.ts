932809a6b18714d449e388523a5f08a0d1e6c78d
Fabricio Liquesley <fabricioliquesley15@gmail.com>
2026-01-30 11:16:09 -0300

    course-platform: create functions to format the duration and level of the course

--- course-platform/src/lib/utils.ts
+++ course-platform/src/lib/utils.ts
@@ -1,6 +1,24 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatDuration(durationInMs: number, showHours = false) {
+  const hours = Math.floor(durationInMs / (60 * 60 * 1000));
+  const minutes = Math.floor((durationInMs % (60 * 60 * 1000)) / (60 * 1000));
+  const seconds = Math.floor((durationInMs % (60 * 1000)) / 1000);
+
+  const formatNumber = (number: number) => number.toString().padStart(2, "0");
+
+  if (hours > 0 || showHours) {
+    return `${formatNumber(hours)}:${formatNumber(minutes)}:${formatNumber(seconds)}`;
+  }
+
+  return `${formatNumber(minutes)}:${formatNumber(seconds)}`;
+}
+
+export function formatLevel(level: string) {
+  return level.charAt(0) + level.slice(1).toLowerCase();
 }
