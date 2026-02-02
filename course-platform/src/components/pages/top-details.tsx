93434407b8657303e58c4f8ddcd950cf5ee1462c
Fabricio Liquesley <fabricioliquesley15@gmail.com>
2026-02-02 14:37:49 -0300

    course-platform: display the current module title and current lesson title in the top details component

--- course-platform/src/components/pages/top-details.tsx
+++ course-platform/src/components/pages/top-details.tsx
@@ -1,14 +1,27 @@
+"use client";
+
 import Link from "next/link";
 import { BackButton } from "../shared/back-button";
-import { Course } from "@/@types/types";
+import { CourseWithModulesAndLessons } from "@/@types/types";
 import { ToggleModuleListCollapsed } from "../shared/toggle-module-list-collapsed";
 import { ToggleAutoplay } from "../shared/toggle-autoplay";
+import { useParams } from "next/navigation";
 
 interface TopDetailsProps {
-  course: Course;
+  course: CourseWithModulesAndLessons;
 }
 
 export function TopDetails({ course }: TopDetailsProps) {
+  const params = useParams();
+
+  const moduleId = params.moduleId as string;
+  const lessonId = params.lessonId as string;
+
+  const currentModule = course.modules.find((module) => module.id === moduleId);
+  const currentLesson = currentModule?.lessons?.find(
+    (lesson) => lesson.id === lessonId,
+  );
+
   return (
     <div className="border-border bg-sidebar sticky top-0 z-10 flex w-full items-center gap-4 border-b p-4 sm:gap-6 sm:p-6">
       <BackButton />
@@ -21,9 +34,9 @@ export function TopDetails({ course }: TopDetailsProps) {
           {course.title}
         </Link>
         <span className="text-muted-foreground">/</span>
-        <p className="line-clamp-1 hidden sm:block">module title</p>
+        <p className="line-clamp-1 hidden sm:block">{currentModule?.title}</p>
         <span className="text-muted-foreground hidden sm:block">/</span>
-        <p className="line-clamp-1">lesson title</p>
+        <p className="line-clamp-1">{currentLesson?.title}</p>
       </div>
 
       <div className="ml-auto flex items-center gap-4">
