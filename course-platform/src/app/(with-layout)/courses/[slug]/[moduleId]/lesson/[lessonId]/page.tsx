9f4cd99760b305d359b39c1e3ec262b052b2faca
Fabricio Liquesley <fabricioliquesley15@gmail.com>
2026-02-02 14:25:03 -0300

    course-platform: change the route to display the current lesson

---
+++ course-platform/src/app/(with-layout)/courses/[slug]/[moduleId]/lesson/[lessonId]/page.tsx
@@ -0,0 +1,31 @@
+import { getCourseBySlug } from "@/actions/courses";
+import { LessonDetails } from "@/components/pages/lesson-details";
+import { ModulesList } from "@/components/pages/modules-list";
+import { TopDetails } from "@/components/pages/top-details";
+import { notFound } from "next/navigation";
+
+interface CoursePageProps {
+  params: Promise<{
+    slug: string;
+  }>;
+}
+
+export default async function CoursePage({ params }: CoursePageProps) {
+  const { slug } = await params;
+
+  const course = await getCourseBySlug(slug);
+
+  if (!course) notFound();
+
+  return (
+    <div className="grid h-screen w-full grid-cols-[1fr_auto] overflow-hidden">
+      <div className="h-full w-full overflow-y-auto">
+        <TopDetails course={course} />
+
+        <LessonDetails lesson={course.modules[0].lessons[0]} />
+      </div>
+
+      <ModulesList modules={course.modules} />
+    </div>
+  );
+}
