3fb2d11caf2536fc0d7dd2db2c2e5d0d14cb6829
Fabricio Liquesley <fabricioliquesley15@gmail.com>
2026-02-02 15:43:13 -0300

    course-platform: get the current and next lesson to move to the lesson details component

--- course-platform/src/app/(with-layout)/courses/[slug]/[moduleId]/lesson/[lessonId]/page.tsx
+++ course-platform/src/app/(with-layout)/courses/[slug]/[moduleId]/lesson/[lessonId]/page.tsx
@@ -7,22 +7,39 @@ import { notFound } from "next/navigation";
 interface CoursePageProps {
   params: Promise<{
     slug: string;
+    moduleId: string;
+    lessonId: string;
   }>;
 }
 
 export default async function CoursePage({ params }: CoursePageProps) {
-  const { slug } = await params;
+  const { slug, moduleId, lessonId } = await params;
 
   const course = await getCourseBySlug(slug);
 
   if (!course) notFound();
+
+  const currentModule = course.modules.find((module) => module.id === moduleId);
+
+  if (!currentModule) notFound();
+
+  const allLessons = course.modules.flatMap((mod) => mod.lessons);
+
+  const currentLessonIndex = allLessons.findIndex(
+    (lesson) => lesson.id === lessonId,
+  );
+
+  const currentLesson = allLessons[currentLessonIndex];
+  const nextLesson = allLessons[currentLessonIndex + 1];
+
+  if (!currentLesson) notFound();
 
   return (
     <div className="grid h-screen w-full grid-cols-[1fr_auto] overflow-hidden">
       <div className="h-full w-full overflow-y-auto">
         <TopDetails course={course} />
 
-        <LessonDetails lesson={course.modules[0].lessons[0]} />
+        <LessonDetails lesson={currentLesson} nextLesson={nextLesson} />
       </div>
 
       <ModulesList modules={course.modules} />
--- course-platform/src/components/pages/lesson-details/index.tsx
+++ course-platform/src/components/pages/lesson-details/index.tsx
@@ -3,12 +3,13 @@ import { LessonPlayer } from "./lesson-player";
 
 interface LessonDetailsProps {
   lesson: Lesson;
+  nextLesson?: Lesson;
 }
 
-export function LessonDetails({ lesson }: LessonDetailsProps) {
+export function LessonDetails({ lesson, nextLesson }: LessonDetailsProps) {
   return (
     <>
-      <LessonPlayer lesson={lesson} />
+      <LessonPlayer lesson={lesson} nextLesson={nextLesson} />
 
       <div className="flex flex-col gap-6 p-6">
         <p className="text-muted-foreground">{lesson.description}</p>
