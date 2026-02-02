dbe9ff08d06115ca790575e175e49c95b89e0d94
Fabricio Liquesley <fabricioliquesley15@gmail.com>
2026-02-02 12:04:40 -0300

    course-platform: create server functions to mark the lesson as complete and get the course progress

--- course-platform/src/actions/course-progress.ts
+++ course-platform/src/actions/course-progress.ts
@@ -2,6 +2,7 @@
 
 import { prisma } from "@/lib/prisma";
 import { getUser } from "./user";
+import { getCourseWithoutModulesAndLessons } from "./courses";
 
 interface MarkLessonAsCompletedPayload {
   courseSlug: string;
@@ -14,13 +15,7 @@ export async function markLessonAsCompleted({
 }: MarkLessonAsCompletedPayload) {
   const { id: userId } = await getUser();
 
-  const course = await prisma.course.findUnique({
-    where: {
-      slug: courseSlug,
-    },
-  });
-
-  if (!course) throw new Error("Course not found");
+  const course = await getCourseWithoutModulesAndLessons(courseSlug);
 
   const isAlreadyCompleted = await prisma.completedLesson.findFirst({
     where: {
@@ -40,4 +35,65 @@ export async function markLessonAsCompleted({
   });
 
   return completedLesson;
+}
+
+export async function unmarkLessonAsCompleted(lessonId: string) {
+  const { id: userId } = await getUser();
+
+  const completedLesson = await prisma.completedLesson.findFirst({
+    where: {
+      lessonId,
+      userId,
+    },
+  });
+
+  if (!completedLesson) return;
+
+  await prisma.completedLesson.delete({
+    where: {
+      id: completedLesson.id,
+    },
+  });
+}
+
+export async function getCourseProgress(courseSlug: string) {
+  const { id: userId } = await getUser();
+
+  const course = await prisma.course.findUnique({
+    where: {
+      slug: courseSlug,
+    },
+    include: {
+      modules: {
+        select: {
+          lessons: {
+            select: {
+              id: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  if (!course) throw new Error("Course not found");
+
+  const completedLessons = await prisma.completedLesson.findMany({
+    where: {
+      courseId: course.id,
+      userId,
+    },
+  });
+
+  const totalLessons = course.modules.flatMap(
+    (mod) => mod.lessons.length,
+  ).length;
+  const completedLessonsCount = completedLessons.length;
+
+  const progress = Math.round((completedLessonsCount / totalLessons) * 100);
+
+  return {
+    completedLessons,
+    progress,
+  };
 }
