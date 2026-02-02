79cfa822ce1e11131ef75574afac8529b74d7187
Fabricio Liquesley <fabricioliquesley15@gmail.com>
2026-02-02 11:58:07 -0300

    course-platform: add a type for completed lessons

--- course-platform/src/@types/types.d.ts
+++ course-platform/src/@types/types.d.ts
@@ -2,6 +2,8 @@ export type Course = import("@/generated/prisma/client").Course;
 export type Tags = import("@/generated/prisma/client").CourseTag;
 export type Module = import("@/generated/prisma/client").CourseModule;
 export type Lesson = import("@/generated/prisma/client").CourseLesson;
+export type CompletedLesson =
+  import("@/generated/prisma/client").CompletedLesson;
 
 export type CourseWithTagsAndModules = Course & {
   tags: Tag[];
