a46ed4bea442535e9c2c4b85ecf00280f9ea38b2
Fabricio Liquesley <fabricioliquesley15@gmail.com>
2026-02-02 15:31:01 -0300

    course-platform: create the logic to mark the lesson as complete and redirect to the next lesson, if any, and if autoplay is enabled

--- course-platform/src/components/pages/lesson-details/lesson-player.tsx
+++ course-platform/src/components/pages/lesson-details/lesson-player.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import { Lesson } from "@/@types/types";
+import { markLessonAsCompleted } from "@/actions/course-progress";
+import { queryKeys } from "@/constants/query-keys";
 import { usePreferencesStore } from "@/stores/preferences";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import dynamic from "next/dynamic";
+import { useParams, useRouter } from "next/navigation";
 
 const VideoPlayer = dynamic(() => import("./video-player"), {
   ssr: false,
@@ -10,16 +14,55 @@ const VideoPlayer = dynamic(() => import("./video-player"), {
 
 interface LessonPlayerProps {
   lesson: Lesson;
+  nextLesson?: Lesson;
 }
 
-export function LessonPlayer({ lesson }: LessonPlayerProps) {
+export function LessonPlayer({ lesson, nextLesson }: LessonPlayerProps) {
+  const params = useParams();
+  const router = useRouter();
+
+  const courseSlug = params.slug as string;
+
+  const queryClient = useQueryClient();
+
   const autoplay = usePreferencesStore((state) => state.autoplay);
+  const setExpandedModule = usePreferencesStore(
+    (state) => state.setExpandedModule,
+  );
+
+  const { mutateAsync: completeLesson } = useMutation({
+    mutationFn: () =>
+      markLessonAsCompleted({ courseSlug, lessonId: lesson.id }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: queryKeys.courseProgress(courseSlug),
+      });
+    },
+  });
 
   const videoId = lesson.videoId;
 
+  const handleEndLesson = async () => {
+    await completeLesson();
+
+    if (!autoplay || !nextLesson) return;
+
+    if (nextLesson.moduleId !== lesson.moduleId) {
+      setExpandedModule(nextLesson.moduleId);
+    }
+
+    router.push(
+      `/courses/${courseSlug}/${nextLesson.moduleId}/lesson/${nextLesson.id}`,
+    );
+  };
+
   return (
     <div key={videoId} className="aspect-video w-full overflow-hidden bg-black">
-      <VideoPlayer videoId={videoId} autoplay={autoplay} />
+      <VideoPlayer
+        videoId={videoId}
+        autoplay={autoplay}
+        onEnd={handleEndLesson}
+      />
     </div>
   );
 }
