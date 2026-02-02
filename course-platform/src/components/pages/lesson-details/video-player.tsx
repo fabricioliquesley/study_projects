8832a0fe534af8bbe15b93945e23655337d843cc
Fabricio Liquesley <fabricioliquesley15@gmail.com>
2026-02-02 15:26:27 -0300

    course-platform: add onEnd property to the video player component

--- course-platform/src/components/pages/lesson-details/video-player.tsx
+++ course-platform/src/components/pages/lesson-details/video-player.tsx
@@ -11,9 +11,10 @@ import {
 interface VideoPlayerProps {
   videoId: string;
   autoplay: boolean;
+  onEnd?: () => void;
 }
 
-function VideoPlayer({ videoId, autoplay }: VideoPlayerProps) {
+function VideoPlayer({ videoId, autoplay, onEnd }: VideoPlayerProps) {
   const userAlreadyInteracted = navigator.userActivation.hasBeenActive;
 
   return (
@@ -21,6 +22,7 @@ function VideoPlayer({ videoId, autoplay }: VideoPlayerProps) {
       title="class video"
       src={`https://www.youtube.com/${videoId}`}
       autoPlay={autoplay && userAlreadyInteracted}
+      onEnd={onEnd}
     >
       <MediaProvider />
       <PlyrLayout icons={plyrLayoutIcons} />
