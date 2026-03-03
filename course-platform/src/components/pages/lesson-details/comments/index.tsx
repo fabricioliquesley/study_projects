0b529de1cddc31374e25629958048d1bdc6363df
Fabricio Liquesley <fabricioliquesley15@gmail.com>
2026-03-03 13:16:58 -0300

    course-platform: render CommentList in LessonComments component

--- course-platform/src/components/pages/lesson-details/comments/index.tsx
+++ course-platform/src/components/pages/lesson-details/comments/index.tsx
@@ -1,10 +1,13 @@
 import { CommentInput } from "./comment-input";
+import { CommentList } from "./comment-list";
 
 export function LessonComments() {
   return (
     <div className="flex flex-col gap-6">
       <div className="text-lg font-semibold">LessonComments</div>
+
       <CommentInput />
+      <CommentList />
     </div>
   );
 }
