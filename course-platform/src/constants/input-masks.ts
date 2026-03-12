498002a841917d3889ed0d813764323fd0669305
Fabricio Liquesley <fabricioliquesley15@gmail.com>
2026-03-12 14:23:31 -0300

    course-platform: add reusable input mask constants for cpf, phone, card, cvv, and cep

---
+++ course-platform/src/constants/input-masks.ts
@@ -0,0 +1,20 @@
+const cpfMask = "___.___.___-__";
+
+const phoneMask = "+55 (__) _____-____";
+
+const cardNumberMask = "____ ____ ____ ____";
+
+const cardValidThruMask = "__/__";
+
+const cvvMask = "___";
+
+const cepMask = "_____-___";
+
+export {
+  cpfMask,
+  phoneMask,
+  cardNumberMask,
+  cardValidThruMask,
+  cvvMask,
+  cepMask,
+};
